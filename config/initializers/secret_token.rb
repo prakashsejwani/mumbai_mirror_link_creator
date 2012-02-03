f108e5089af07c821e8c4717d4600b04aca32033
Prakash Sejwani <prakash.sejwani@livialegal.com>
2012-02-03 21:44:04 +0530

    replaced with plain Html and js from rails framework

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-MumbaiMirrorLinkCreator::Application.config.secret_token = '9a7213a22bc92371d3aa314a4182616c43b5413c3eef49165b6f986a3cd56a353ce8c70f67f9a85eefb7165a14bbe888109dd82461ee6c87c1932f5f23bfdae0'
--- test/unit/helpers/link_creators_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class LinkCreatorsHelperTest < ActionView::TestCase
-end
